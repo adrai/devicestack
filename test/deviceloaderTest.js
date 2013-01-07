d804653563d75b9e737815ebb7902d4726397f58
Adriano Raiano <araiano@ksw.kaba.com>
2013-01-07 15:11:50 +0100

    use recursive nextTick instead of setInterval

--- test/deviceloaderTest.js
+++ test/deviceloaderTest.js
@@ -61,7 +61,7 @@ describe('DeviceLoader', function() {
         deviceloader.once('plug', function(device) {
           done();
         });
-        deviceloader.startLookup(10);
+        deviceloader.startLookup();
 
       });
 
@@ -70,7 +70,7 @@ describe('DeviceLoader', function() {
         deviceloader.once('unplug', function(device) {
           done();
         });
-        deviceloader.startLookup(10);
+        deviceloader.startLookup();
         setTimeout(function() {
           deviceloader.startDevices = [deviceloader.startDevices.slice(0, 1)];
         }, 20);
@@ -96,7 +96,7 @@ describe('DeviceLoader', function() {
             done();
           }, 20);
         });
-        deviceloader.startLookup(10);
+        deviceloader.startLookup();
 
       });
 
